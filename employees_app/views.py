e9ad7255e9b44073bd9fffa9c8f8a08ab41a29fb
Philipp Biebert <philipp.biebert@gmail.com>
2026-04-24 12:22:10 +0200

    first commit

--- employees_app/views.py
+++ employees_app/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from .models import Employee
-from django.db.models import Avg, Q
+from django.db.models import Avg, Q, Count
 from datetime import date
 
 
 def employee_overview(request):
 
     # Hier die entsprechenden Filter anlegen und die context-Variable definieren, um die Daten an das Template zu übergeben
+    worker_list = Employee.objects.values(
+        'name', 'salary', 'department__name')
 
-    return render(request, 'employee_list.html')
+    hight_salary = Employee.objects.all().filter(
+        salary__gt=3000).values('name', 'salary', 'department__name')
+
+    number_hight_salary = Employee.objects.all().filter(
+        salary__gte=5000).count()
+
+    all_emloyee_sales = Employee.objects.all().filter(
+        department__name='Sales').aggregate(Avg('salary'))
+
+    employees_before_2021 = Employee.objects.filter(
+        hire_date__lte=date(2021, 1, 1)
+    ).exclude(department__name='HR').values('name', 'department__name', 'hire_date')
+    print(employees_before_2021)
+
+    return render(request, 'employee_list.html', {'worker_list': worker_list, 'hight_salary': hight_salary, 'number_hight_salary': number_hight_salary, 'all_emloyee_sales': all_emloyee_sales, 'employees_before_2021': employees_before_2021})
